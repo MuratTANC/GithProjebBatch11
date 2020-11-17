b6d3b1b6e0080786247198c5c040b57f7cde2785
MuratTANC <71972158+MuratTANC@users.noreply.github.com>
2020-11-17 22:56:48 +0300

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -2,8 +2,8 @@ public class BirinciGun {
 
 	public static void main(String[] args) {
 	
-		System.out.println("22:07 - Murat TANC Birinci Gün");
-		System.out.println("ikinci Gün");
+		System.out.println("22:07 - Murat TANC Birinci Gun");
+		System.out.println("ikinci Gun");
 		System.out.println("Murat TANC01");
 		System.out.println("Murat TANC02");
 		System.out.println("Murat TANC03");
